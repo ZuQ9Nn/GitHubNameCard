075440e86a28465120878b98f5d2c3aeef8c2472
kazuki_tanaka <ZuQ9Nn@gmail.com>
2013-02-03 09:00:23 +0900

    Initial Commit

---
+++ GitHubNameCard/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitHubNameCard
+//
+//  Created by kazuki_tanaka on 2013/02/03.
+//  Copyright (c) 2013年 ZuQ9Nn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitHubNameCard/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GitHubNameCard
+//
+//  Created by kazuki_tanaka on 2013/02/03.
+//  Copyright (c) 2013年 ZuQ9Nn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
